7b05284d7ea5a3fe7d49980e5efc253174801507
Dominic Cassidy <dominic@cassidy.casa>
2026-05-12 15:21:14 +0100

    feat: option to clear file on close

--- bin.js
+++ bin.js
@@ -14,13 +14,15 @@ const DEFAULT_FILE = path.join('.', 'bugbear.jsonl')
 const cmd = command(
   'bugbear',
   arg('[file]', 'jsonl log file to browse'),
-  flag('--file|-f [path]', 'jsonl log file to browse')
+  flag('--file|-f [path]', 'jsonl log file to browse'),
+  flag('--clear', 'clear the log file on close')
 )
 
 const parsed = cmd.parse(argv())
 if (parsed === null) process.exit(0)
 
 const file = parsed.flags.file || parsed.args.file || DEFAULT_FILE
+const clearFileOnClose = parsed.flags.clear
 
 const root = new LabeledList()
 const scopeMap = new Map()
@@ -79,7 +81,7 @@ function loadNew() {
 }
 
 loadNew()
-const nav = navigate(root)
+const nav = navigate(root, clearFileOnClose, file)
 
 try {
   fs.watch(file, () => {
